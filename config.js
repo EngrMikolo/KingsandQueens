a4668eaed5235ccadae6fc1cb1b3ac63f412763e
Engr_mikolo <ogunderoayodeji@gmail.com>
2018-03-19 00:33:28 +0100

    sync

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
 module.exports = {
-
-	// "database": "mongodb://root:abc123@ds017173.mlab.com:17173/mystory",
-	"port": process.env.PORT || 3000,
-	// "secretKey": "YourSecretKey"
-}
+"port": process.env.PORT || 3000
+};
